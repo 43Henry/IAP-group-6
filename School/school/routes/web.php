edba646cee1381db2d8c108c3a0cb5ed0faa8204
Nyakundi <henrymoire@gmail.com>
2022-12-31 20:24:47 +0300

    Moved files, started work on login page
    I moved the ready views and stylesheets into their respective folders, then started working on getting the login page working. Ran into a few errors so it doesn't work yet.

--- School/school/routes/web.php
+++ School/school/routes/web.php
@@ -20,3 +20,52 @@ Route::get('/', function ()
 });
 
 Route::get('student', [StudentController::class, 'index']);
+
+Route::get('home', function()
+{
+    return view('LANDING-PAGE');
+});
+
+Route::get('loginTrial', function()
+{
+    return view('loginTrial');
+});
+
+Route::post('logincheck', function()
+{
+    //username and password validation rule
+    $rules = array(
+        'username' => 'required',
+        'password' => 'required',
+    );
+    $v = Validator::make(Input::all (), $rules);
+    if($v -> fails())
+    {
+        //username or password missing
+        //validation fails
+        //used to retain input values
+        Input::flash();
+        //return to login page with errors
+        return Redirect::to('loginTrial')
+        ->withInput()
+        ->withErrors($v->messages ());
+    } 
+    else
+    {
+        $userdata = array(
+            'username' => Input::get('username'),
+            'password' => Input::get('password')
+        );
+        If(Auth::attempt($userdata))
+        {
+            //authentication success, enters home page
+            return Redirect::to('home');
+        }
+        else
+        {
+            //authentication fail, back to login page with errors
+            return Redirect::to('loginTrial')
+            ->withErrors('Incorrect login details');
+        }
+    }
+});
